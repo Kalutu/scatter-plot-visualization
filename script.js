ca87de34240dd805b58473846f575220ca78fb02
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-06-28 14:34:47 +0300

    modified:   script.js

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
 let url = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json";
 let req = new XMLHttpRequest();
 let values = [];
+let xScale;
+let yScale;
+let width = 800;
+let height = 600;
+let padding = 40;
+let svg = d3.select("svg");
+
+let drawCanvas= () =>{
+    svg.attr("width",width);
+    svg.attr("height",height);
+}
+
+let generateScales = () =>{
+
+}
+
+let drawPoints = ()=>{
+
+}
+
+let generateAxis = () =>{
+
+}
