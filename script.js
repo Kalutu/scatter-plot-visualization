eae66eb79cdbba289fd672756e33696d42f47ab8
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-06-28 15:26:52 +0300

    modified:   script.js

--- script.js
+++ script.js
@@ -15,20 +15,33 @@ let drawCanvas= () =>{
 
 let generateScales = () =>{
     xScale = d3.scaleLinear()
+                .domain([d3.min(values,item=>item["Year"]),d3.max(values,item=>item["Year"])])
                .range([padding,width-padding])
 
     yScale = d3.scaleTime()
+                .domain([d3.min(values,item=> new Date(item["Seconds"]*1000)),d3.max(values,item=> new Date(item["Seconds"]*1000))])
                 .range([padding,height-padding])
 }
 
 let drawPoints = ()=>{
-
+    svg.selectAll("circle")
+        .data(values)
+        .enter()
+        .append("circle")
+        .attr("class","dot")
+        .attr("r","5")
+        .attr("data-xvalue",item=>item["Year"])
+        .attr("data-yvalue",item=> new Date(item["Seconds"]*1000))
+        .attr("cx",item=>xScale(item["Year"]))
+        .attr("cy",item=>yScale(new Date(item["Seconds"]*1000)))
 }
 
 let generateAxis = () =>{
-    let xAxis = d3.axisBottom(xScale);
+    let xAxis = d3.axisBottom(xScale)
+                  .tickFormat(d3.format("d"));
 
-    let yAxis = d3.axisLeft(yScale);
+    let yAxis = d3.axisLeft(yScale)
+                  .tickFormat(d3.timeFormat("%M:%S"));
 
     svg.append("g")
         .call(xAxis)
